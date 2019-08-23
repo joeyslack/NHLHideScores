bae430275d0b650f1e3d1c8c7c1ba2cd8a4bb9ac
Joseph Slack <josephdslack@gmail.com>
2019-08-23 14:37:40 -0700

    1.2 - block series details

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# NHLHideScores
+# NHL Hide Scores
+
+Hide the scores on NHL.com, both past and present. Excellent for NHL Gamecenter and nhl.tv subscribers. Hides scores from the website.
+
+If you're like me, you subscribe to nhl.tv, and sometimes you watch the game hours, or days later and don't want to be spoiled. On the iOS app there is a setting to hide scores, but on the web there isn't one. 
+
+This extension will hide the scores when you go to nhl.tv, allowing you to still hover over the scoreboards at the top and choose the game to watch, even when scrolling left, to previous days.
+
+Comments & Feedback appreciated, happy to update extension when necessary. Just providing this extension to help fellow hockey fans out. There are no ads or payments for this. The code is open sourced, and it's 40kb of code & images - so it's tiny.
+
+# Contributing
+
+Feel free to send pull requests...
--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({hide: true});
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.nhl.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+
+});
+
+
 let hideScores = document.getElementById('hideScores');
 let showScores = document.getElementById('showScores');
 
@@ -7,7 +22,7 @@ hideScores.onclick = function(element) {
     chrome.tabs.executeScript(null, {file: 'jquery.min.js'}, function() {
     	chrome.tabs.executeScript(
     		tabs[0].id,
-    		{code: '$(".g5-component--nhl-scores__team-score").hide().css("visibility", "hidden");'}
+    		{code: '$(".g5-component--nhl-scores__team-score, .g5-component--nhl-scores__status-series").hide().css("visibility", "hidden");'}
     	);}
     );
   });
@@ -19,7 +34,7 @@ showScores.onclick = function(element) {
     chrome.tabs.executeScript(null, {file: 'jquery.min.js'}, function() {
     	chrome.tabs.executeScript(
     		tabs[0].id,
-    		{code: '$(".g5-component--nhl-scores__team-score").show().css("visibility", "visible");'}
+    		{code: '$(".g5-component--nhl-scores__team-score, .g5-component--nhl-scores__status-series").show().css("visibility", "visible");'}
     	);}
     );
   });
