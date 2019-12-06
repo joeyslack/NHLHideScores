8f14c2269dbf33fa5d5eb58bf1360dc459a6dc08
Joseph Slack <josephdslack@gmail.com>
2019-12-06 14:11:06 -0800

    Update to 1.3. Includes 'FINAL' removal, clean up hide and show, add news filters

--- popup.js
+++ popup.js
@@ -1,41 +1,28 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({hide: true});
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.nhl.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-
-});
-
-
 let hideScores = document.getElementById('hideScores');
 let showScores = document.getElementById('showScores');
+let hideNews = document.getElementById('hideNews');
+let showNews = document.getElementById('showNews');
 
 hideScores.onclick = function(element) {
-	// chrome.storage.sync.set({hide: true});
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(null, {file: 'jquery.min.js'}, function() {
-    	chrome.tabs.executeScript(
-    		tabs[0].id,
-    		{code: '$(".g5-component--nhl-scores__team-score, .g5-component--nhl-scores__status-series").hide().css("visibility", "hidden");'}
-    	);}
-    );
+    chrome.tabs.executeScript(tabs[0].id, {code: 'document.getElementsByTagName("BODY")[0].classList.remove("showScores");'});
   });
 };
 
 showScores.onclick = function(element) {
-	// chrome.storage.sync.set({hide: false});
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(null, {file: 'jquery.min.js'}, function() {
-    	chrome.tabs.executeScript(
-    		tabs[0].id,
-    		{code: '$(".g5-component--nhl-scores__team-score, .g5-component--nhl-scores__status-series").show().css("visibility", "visible");'}
-    	);}
-    );
+    chrome.tabs.executeScript(tabs[0].id, {code: 'document.getElementsByTagName("BODY")[0].classList.add("showScores");'});
+  });
+}
+
+showNews.onclick = function(element) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id,{code: 'document.getElementsByTagName("BODY")[0].classList.add("showNews");'});
+  });
+}
+
+hideNews.onclick = function(element) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {code: 'document.getElementsByTagName("BODY")[0].classList.remove("showNews");'});
   });
 }
